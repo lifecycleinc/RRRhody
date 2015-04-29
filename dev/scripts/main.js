1990e36d5f96f806c815198197da92e2f68d18f1
Jeff Lupinski <lupinskij@gmail.com>
2015-04-29 17:31:39 -0400

    Add equalize

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,20 @@
 $(function() {
-  console.log('rrrhody.org');
+  var setHeight = function() {
+   var heights, itemInfos, maxHeight;
+
+    equalize = this.$('.equalize');
+    equalize.height('auto');
+
+    heights = _.map(equalize, function(itemInfo) {
+      return $(itemInfo).height();
+    });
+
+    maxHeight = _.max(heights);
+
+    return equalize.height(maxHeight);
+  };
+
+  setHeight();
+
+  $(window).on('resize', _.debounce(setHeight, 150));
 });
