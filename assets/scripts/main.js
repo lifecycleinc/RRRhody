0806c60380439612120e8629caede320e5afbf08
Seth Krasnianski <seth@mojotech.com>
2016-02-23 15:23:48 -0500

    Remove boilerplate js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,1 @@
-import $ from 'jquery';
-import {date, hello} from './utilities';
-
-hello('RRRhody');
-
-console.info(`The current date is ${date()}`);
-console.info(`The current time is ${date('time')}`);
-console.info(`The current date and time is ${date('full')}`);
+console.log('RRRhody');
